b36f2a2e3867d2450a21106cbc57fd98b5a77bc1
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-09-16 08:35:27 +0900

    Time: 135 ms (84.36%), Space: 40.6 MB (62.19%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,31 @@
 class Solution {
-    boolean[][] visited; 
+    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int columns = board[0].length;
-        visited = new boolean[rows][columns];
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                if(word.charAt(0)==board[i][j]&&search(board,word,i,j,0)){
+        visited= new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(word.charAt(0)==board[i][j]&&search(board,word,0,i,j)){
                     return true;
                 }
             }
         }
         return false;
     }
-
-    private boolean search(char[][]board, String word, int i, int j, int index){
-        if(index==word.length()){
+    public boolean search(char[][] board, String word, int index, int i,int j){
+        if(word.length()==index){
             return true;
         }
-        if(i<0||i>=board.length||j<0||j>=board[0].length||board[i][j]!=word.charAt(index)||visited[i][j]){
+        if(i<0||i>=board.length||j<0||j>=board[0].length||word.charAt(index)!=board[i][j]||visited[i][j]){
             return false;
         }
-        visited[i][j] = true;
-        if(search(board,word,i-1,j,index+1)||
-           search(board,word,i+1,j,index+1)||
-           search(board,word,i,j-1,index+1)||
-           search(board,word,i,j+1,index+1)){
-            return true;
-        }
-        visited[i][j] = false;
+        visited[i][j]=true;
+        if(search(board,word,index+1,i-1,j)||
+            search(board,word,index+1,i+1,j)||
+            search(board,word,index+1,i,j-1)||
+            search(board,word,index+1,i,j+1)){
+                return true;
+            }
+        visited[i][j]= false;
         return false;
     }
 }
