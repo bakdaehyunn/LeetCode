5c2b36adbfd376022322f47dc07761dcfad05f03
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-09-19 10:06:00 +0900

    Time: 15 ms (16.65%), Space: 44 MB (16.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen  = new HashSet<>();
+        Set<String> seen  = new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char currentVal = board[i][j];
