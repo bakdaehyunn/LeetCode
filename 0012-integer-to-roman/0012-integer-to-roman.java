9b70f71a8c44da8029e254fd0a6b074e7bea8a05
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-09-22 16:46:44 +0900

    Time: 3 ms (98.13%), Space: 43 MB (82.94%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,13 +3,11 @@ class Solution {
         int[] intCode = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] code={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"} ;
         StringBuilder sb = new StringBuilder();
-        int i=0;
-        while(num>0){
+        for(int i=0;i<intCode.length;i++){
           while(num >=intCode[i]){
             sb.append(code[i]);
             num-=intCode[i];
           }
-          i++;
         } 
         return sb.toString();
     }
