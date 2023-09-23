b8595e01e943e1f4de6f6ed4378ac4ef0c84e3fd
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-09-23 17:22:59 +0900

    Time: 24 ms (100%), Space: 41.9 MB (55.92%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,19 +20,19 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if(node == null) return null;
-        Node copy= new Node(node.val);
+        if(node==null) return null;
+        Node copy = new Node(node.val);
         Node[] visited = new Node[101];
         dfs(node,copy,visited);
         return copy;
     }
     public void dfs(Node node,Node copy,Node[] visited){
-        visited[copy.val] =copy;
+        visited[copy.val]=copy;
         for(Node n : node.neighbors){
             if(visited[n.val]==null){
                 Node newNode = new Node(n.val);
                 copy.neighbors.add(newNode);
-                dfs(n, newNode,visited);
+                dfs(n,newNode,visited);
             }else{
                 copy.neighbors.add(visited[n.val]);
             }
