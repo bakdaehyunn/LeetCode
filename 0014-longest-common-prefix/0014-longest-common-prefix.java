19c7a682acf0014130445c73ebfca4df11595f60
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-06 19:09:57 +0900

    Time: 1 ms (82.94%), Space: 40.5 MB (39.04%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,14 +3,14 @@ class Solution {
         Arrays.sort(strs);
         String first = strs[0];
         String last = strs[strs.length-1];
-        int idx = 0;
-        while(idx<first.length() && idx< last.length()){
-            if(first.charAt(idx)==last.charAt(idx)){
-                idx++;
+        int index=0;
+        while(index<first.length()&&index<last.length()){
+            if(first.charAt(index)==last.charAt(index)){
+                index++;
             }else{
                 break;
             }
         }
-        return first.substring(0,idx); 
+        return first.substring(0,index);
     }
 }
