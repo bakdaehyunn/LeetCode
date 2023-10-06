5f44a27b7c18c5e8bce92a353d4496a7c02b6eef
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-06 11:06:12 +0900

    Time: 0 ms (100%), Space: 40.5 MB (30.95%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,20 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int  n = citations.length;
-        int[] buckets = new int[n+1];
-        for(int c : citations){
-          if(c>=n){
-            buckets[n]++;
-          }else{
-            buckets[c]++;
-          }
+        int n= citations.length;
+        int[] bucket = new int[n+1];
+        for(int c:citations){
+            if(c>=n){
+                bucket[n]++;
+            }else{
+                bucket[c]++;
+            }
         }
         int count=0;
-        for(int i= n;i>=0;i--){
-          count+=buckets[i];
-          if(count>=i){
-            return i;
-          }
+        for(int i=n;i>0;i--){
+            count +=bucket[i];
+            if(count>=i){
+                return i;
+            }
         }
         return 0;
     }
