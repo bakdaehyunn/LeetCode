e42c248a079e54859b69c2978636194c22496a86
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-07 13:38:46 +0900

    Time: 4 ms (75.46%), Space: 44.6 MB (22.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,23 +2,22 @@ class Solution {
     public String convert(String s, int numRows) {
         StringBuilder[] sb = new StringBuilder[numRows];
         for(int i=0;i<numRows;i++){
-          sb[i] = new StringBuilder();
+            sb[i] = new StringBuilder();
         }
-        int index=0;
-
+        int index =0;
         while(index<s.length()){
-          for(int i=0;i<numRows &&index<s.length();i++){
-            sb[i].append(s.charAt(index));
-            index++;
-          }
-          for(int i=numRows-2;i>0&&index<s.length();i--){
-            sb[i].append(s.charAt(index));
-            index++;
-          }
+            for(int i=0;i<numRows&&index<s.length();i++){
+                sb[i].append(s.charAt(index));
+                index++;
+            }
+            for(int i=numRows-2;i>0&&index<s.length();i--){
+                sb[i].append(s.charAt(index));
+                index++;
+            }
         }
-        StringBuilder result = new StringBuilder();
-        for(int i=0;i<sb.length;i++){
-          result.append(sb[i]);
+        StringBuilder result =new StringBuilder();
+        for(int i=0;i<numRows;i++){
+            result.append(sb[i]);
         }
         return result.toString();
     }
