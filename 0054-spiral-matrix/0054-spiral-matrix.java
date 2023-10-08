305244a5d8d6b818dff75d56c80b2c5a562f357e
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-08 17:46:42 +0900

    Time: 0 ms (100%), Space: 41.1 MB (10.14%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> list =new ArrayList<>();
-        int rowBegin=0;
+        List<Integer> result = new ArrayList<>();
+        int rowStart= 0;
         int rowEnd = matrix.length-1;
-        int colBegin =0;
-        int colEnd=matrix[0].length-1;
-        while(rowBegin<=rowEnd&&colBegin<=colEnd){
-            for(int j=colBegin;j<=colEnd;j++){
-                list.add(matrix[rowBegin][j]);
+        int colStart = 0;
+        int colEnd = matrix[0].length-1;
+        while(rowStart<=rowEnd&&colStart<=colEnd){
+            for(int j=colStart;j<=colEnd;j++){
+                result.add(matrix[rowStart][j]);
             }
-            rowBegin++;
-            for(int i=rowBegin;i<=rowEnd;i++){
-                list.add(matrix[i][colEnd]);
+            rowStart++;
+            for(int i=rowStart;i<=rowEnd;i++){
+                result.add(matrix[i][colEnd]);
             }
             colEnd--;
-            if(rowBegin<=rowEnd){
-                for(int j=colEnd;j>=colBegin;j--){
-                    list.add(matrix[rowEnd][j]);
+            if(rowStart<=rowEnd){
+                for(int j=colEnd;j>=colStart;j--){
+                    result.add(matrix[rowEnd][j]);
                 }
                 rowEnd--;
             }
-            if(colBegin<=colEnd){
-                for(int i=rowEnd;i>=rowBegin;i--){
-                    list.add(matrix[i][colBegin]);
+            if(colStart<=colEnd){
+                for(int i=rowEnd;i>=rowStart;i--){
+                    result.add(matrix[i][colStart]);
                 }
-                colBegin++;
+                colStart++;
             }
         }
-        return list;
+        return result;
     }
 }
