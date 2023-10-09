a59b9ee16505d2c9876769a5ea8cadd88b06f5cf
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-09 17:46:49 +0900

    Time: 1 ms (82.52%), Space: 40.2 MB (68.58%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] strs = s.split(" ");
-        if(pattern.length()!=strs.length){
+        if(strs.length!=pattern.length()){
             return false;
         }
-        HashMap<Character,String> charToStr = new HashMap<>();
-        HashMap<String,Character> strToChar = new HashMap<>();
-        for(int i=0;i<pattern.length();i++){
-            char c = pattern.charAt(i);
-            String str = strs[i];
-            
-            if(!charToStr.containsKey(c)){
-                charToStr.put(c,str);
+        Map<Character,String> charToStr = new HashMap<>();
+        Map<String,Character> strToChar = new HashMap<>();
+        for(int i=0;i< pattern.length();i++){
+            if(!charToStr.containsKey(pattern.charAt(i))){
+                charToStr.put(pattern.charAt(i),strs[i]);
             }
-            if(!strToChar.containsKey(str)){
-                strToChar.put(str,c);
+            if(!strToChar.containsKey(strs[i])){
+                strToChar.put(strs[i],pattern.charAt(i));
             }
-            if(!charToStr.get(c).equals(str)||!strToChar.get(str).equals(c)){
+            if(!charToStr.get(pattern.charAt(i)).equals(strs[i])||!strToChar.get(strs[i]).equals(pattern.charAt(i))){
                 return false;
             }
         }
