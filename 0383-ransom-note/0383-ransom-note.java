6fce218f52ec720c2df8fb767ff84c7b90b38e85
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-09 16:56:16 +0900

    Time: 15 ms (32.27%), Space: 44.2 MB (20.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer> map  =new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
         for(char c:magazine.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
