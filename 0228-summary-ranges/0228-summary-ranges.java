9a90d9aebafcd84af8080ca32f43d42dbefe0cf6
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-10 12:03:07 +0900

    Time: 7 ms (15.13%), Space: 40.8 MB (49.12%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,8 +2,8 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            int start = nums[i];
-            while(i+1<nums.length&&nums[i]+1==nums[i+1]){
+            int start= nums[i];
+            while(i+1<nums.length&&nums[i]+1 ==nums[i+1]){
                 i++;
             }
             if(start!=nums[i]){
