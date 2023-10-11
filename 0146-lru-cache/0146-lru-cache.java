e948b7acc0201a0dad2016f4a42e3b6009ae8278
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-11 14:25:06 +0900

    Time: 47 ms (40.24%), Space: 127.9 MB (17.03%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -9,22 +9,22 @@ class LRUCache {
             this.value = value;
         }
     }
-    int capacity;
     Map<Integer,Node> map;
+    int capacity;
     Node head;
     Node tail;
     public LRUCache(int capacity) {
-        map = new HashMap<>();
         this.capacity = capacity;
+        map = new HashMap<>();
         head = new Node(0,0);
         tail = new Node(0,0);
-        head.next = tail;
+        head.next= tail;
         tail.prev = head;
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
-            Node node  = map.get(key);
+            Node node = map.get(key);
             remove(node);
             insert(node);
             return node.value;
@@ -41,15 +41,15 @@ class LRUCache {
         }
         insert(new Node(key,value));
     }
-    void remove(Node node){
+    public void remove(Node node){
         map.remove(node.key);
-        node.prev.next= node.next;
+        node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-    void insert(Node node){
+    public void insert(Node node){
         map.put(node.key,node);
         Node temp = head.next;
-        head.next= node;
+        head.next = node;
         node.prev = head;
         node.next = temp;
         temp.prev = node;
