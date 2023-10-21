c99e4026ef8ad723829f887f36b6007c0e8129d9
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-22 00:22:08 +0900

    Time: 4 ms (23.24%), Space: 43.4 MB (34.46%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -21,7 +21,7 @@ class Solution {
         if(preStart>preorder.length-1||inStart>inEnd)return null;
         TreeNode root = new TreeNode(preorder[preStart]);
         int inIndex=0;
-        for(int i=0;i<inorder.length;i++){
+        for(int i=inStart;i<=inEnd;i++){
             if(root.val == inorder[i]){
                 inIndex = i;
             }
