f996a347753722e6ade68bf1043d4432ccbacfc3
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-23 16:05:33 +0900

    Time: 1 ms (100%), Space: 43.1 MB (76.26%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,32 +10,31 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-      if(lists==null || lists.length==0) return null;
+        if(lists==null|lists.length==0)return null;
         return helper(lists,0,lists.length-1);
     }
-    public ListNode helper(ListNode[] lists, int left,int right){
-      if(left==right) return lists[left];
-      int mid = left + (right-left)/2;
-      ListNode l1 = helper(lists,left,mid);
-      ListNode r1 = helper(lists,mid+1,right);
-      return merge(l1,r1);
+    public ListNode helper(ListNode[] lists, int left, int right){
+        if(left==right)return lists[left];
+        int mid = left +(right-left)/2;
+        ListNode leftNode = helper(lists,left,mid);
+        ListNode rightNode = helper(lists,mid+1,right);
+        return merge(leftNode,rightNode);
     }
-    public ListNode merge(ListNode l1, ListNode r1){
-      ListNode dummy = new ListNode(0);
-      ListNode current = dummy;
-
-      while(l1!=null&&r1!=null){
-        if(l1.val<r1.val){
-          current.next = l1;
-          l1 = l1.next;
-        }else{
-          current.next = r1;
-          r1 = r1.next;
+    public ListNode merge(ListNode left,ListNode right){
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while(left!=null&&right!=null){
+            if(left.val<right.val){
+                current.next= left;
+                left = left.next;
+            }else{
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
         }
-        current = current.next;
-      }
-      if(l1!=null) current.next = l1;
-      if(r1!=null) current.next = r1;
-      return dummy.next;
+        if(left!=null)current.next = left;
+        if(right!=null) current.next = right;
+        return dummy.next;
     }
 }
