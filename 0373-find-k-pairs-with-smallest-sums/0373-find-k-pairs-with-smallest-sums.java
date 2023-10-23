3c8e15e1feb217c10264480b42f86e6d1d37ef3b
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-23 19:37:54 +0900

    Time: 32 ms (66.02%), Space: 58.5 MB (33.25%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -2,21 +2,19 @@ class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> result = new ArrayList<>();
         PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->a[0]-b[0]);
-
-        for(int x : nums1){
-            queue.offer(new int[]{x+nums2[0],0});
+        for(int num: nums1){
+            queue.offer(new int[]{num+nums2[0],0});
         }
-        while(k>0&&!queue.isEmpty()){
-            int[] pair = queue.poll();
-            int sum = pair[0];
-            int pos = pair[1];
-
-            List<Integer> currentPair = new ArrayList<>();
-            currentPair.add(sum-nums2[pos]);
-            currentPair.add(nums2[pos]);
-            result.add(currentPair);
+        while(k!=0&&!queue.isEmpty()){
+            int[] arr = queue.poll();
+            int sum =  arr[0];
+            int pos = arr[1];
+            List<Integer> list = new ArrayList<>();
+            list.add(sum-nums2[pos]);
+            list.add(nums2[pos]);
+            result.add(list);
             if(pos+1<nums2.length){
-                queue.offer(new int[]{sum - nums2[pos]+nums2[pos + 1],pos+1});
+                queue.offer(new int[]{sum-nums2[pos]+nums2[pos+1],pos+1});
             }
             k--;
         }
