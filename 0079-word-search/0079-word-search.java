ce14a50f7082d146702b22662d2b72d36a08ab12
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-23 15:05:06 +0900

    Time: 139 ms (73.22%), Space: 40.5 MB (66.16%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,27 @@
 class Solution {
     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        visited= new boolean[board.length][board[0].length];
+        visited = new boolean[board.length][board[0].length];
         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(word.charAt(0)==board[i][j]&&search(board,word,0,i,j)){
+            for(int j=0;j<board[i].length;j++){
+                if(word.charAt(0)==board[i][j]&&backtrack(board,word,0,i,j)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean search(char[][] board, String word, int index, int i,int j){
-        if(word.length()==index){
-            return true;
-        }
-        if(i<0||i>=board.length||j<0||j>=board[0].length||word.charAt(index)!=board[i][j]||visited[i][j]){
-            return false;
-        }
-        visited[i][j]=true;
-        if(search(board,word,index+1,i-1,j)||
-            search(board,word,index+1,i+1,j)||
-            search(board,word,index+1,i,j-1)||
-            search(board,word,index+1,i,j+1)){
-                return true;
-            }
-        visited[i][j]= false;
+    public boolean backtrack(char[][] board, String word,int length, int i, int j){
+        if(length==word.length())return true;
+        if(i<0||i>board.length-1||j<0||j>board[i].length-1||word.charAt(length)!=board[i][j]||visited[i][j]){
+    return false;
+        } 
+        visited[i][j]= true;
+        if(backtrack(board,word,length+1,i-1,j)||
+        backtrack(board,word,length+1,i+1,j)||
+        backtrack(board,word,length+1,i,j-1)||
+        backtrack(board,word,length+1,i,j+1))return true;
+        visited[i][j]=false;
         return false;
     }
 }
