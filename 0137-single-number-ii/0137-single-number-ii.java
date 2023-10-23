ad6e75dcedd389dfb1e4f07b9aca07ce4bf1b6f2
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-23 20:08:09 +0900

    Time: 3 ms (75.1%), Space: 43.7 MB (66.33%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+        int result= 0;
         for(int i=0;i<32;i++){
-          int sum=0;
-          for(int j=0;j<nums.length;j++){
-            if(((nums[j]>>i)&1)==1){
-              sum++;
-              sum%=3;
+            int sum = 0;
+            for(int j=0;j<nums.length;j++){
+                if(((nums[j]>>i)&1)==1)sum++;
             }
-          }
-          
-          if(sum!=0){
-            result |= sum<<i;
-          }
+            sum%=3;
+            if(sum!=0){
+                result|=sum<<i;
+            }
         }
         return result;
     }
-    
 }
