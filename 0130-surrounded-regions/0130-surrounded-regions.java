931be1169cec07ff4032e1d2f71d21c6dd066dfb
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-23 11:25:16 +0900

    Time: 2 ms (80.03%), Space: 43.5 MB (90.86%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,31 +1,27 @@
 class Solution {
     public void solve(char[][] board) {
-        if(board.length==0 || board[0].length==0) return;
-        int m = board.length;
-        int n = board[0].length;
-
-        for(int i=0;i<m; i++){
-          dfs(board,i,0);
-          dfs(board,i,n-1);
+        for(int i=0;i<board.length;i++){
+            dfs(board,i,0);
+            dfs(board,i,board[i].length-1);
         }
-        for(int i=0;i<n;i++){
-          dfs(board,0,i);
-          dfs(board,m-1,i);
+        for(int j=0;j<board[0].length;j++){
+            dfs(board,0,j);
+            dfs(board,board.length-1,j);
         }
-        for(int i=0;i<m;i++){
-          for(int j=0;j<n;j++){
-            if(board[i][j]=='O')board[i][j]='X';
-            if(board[i][j]=='*')board[i][j]='O';
-          }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='O')board[i][j]='X';
+                if(board[i][j]=='*')board[i][j]='O';
+            }
         }
     }
-    public void dfs(char[][]board, int i, int j){
-      if(i<0 || i>=board.length || j<0 || j>=board[0].length)return;
-      if(board[i][j]=='X'||board[i][j]=='*')return ;
-      board[i][j]='*';
-      dfs(board,i-1,j);
-      dfs(board,i+1,j);
-      dfs(board,i,j-1);
-      dfs(board,i,j+1);
+    public void dfs(char[][] board,int i, int j){
+        if(i<0||i>board.length-1||j<0||j>board[0].length-1)return ;
+        if(board[i][j]=='X'||board[i][j]=='*')return ;
+        board[i][j] = '*';
+        dfs(board,i-1,j);
+        dfs(board,i+1,j);
+        dfs(board,i,j-1);
+        dfs(board,i,j+1);
     }
 }
