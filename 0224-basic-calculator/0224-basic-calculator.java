0b60639013f3c4fd04bf4a0d3ee4d4c284ea408b
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 10:18:59 +0900

    Time: 10 ms (43.93%), Space: 43.5 MB (89.75%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,19 +1,19 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-        int sum = 0;
-        int sign = 1;
+        int sum=0;
+        int sign=1;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
-                int num = 0;
+                int num=0;
                 while(i<s.length()&&s.charAt(i)>='0'&&s.charAt(i)<='9'){
                     num = num*10+s.charAt(i)-'0';
                     i++;
                 }
-                sum += sign*num;
+                sum+=sign*num;
                 i--;
-            }else if( s.charAt(i)=='+'){
-                sign=1;
+            }else if(s.charAt(i)=='+'){
+                sign = 1;
             }else if(s.charAt(i)=='-'){
                 sign =-1;
             }else if(s.charAt(i)=='('){
@@ -22,8 +22,8 @@ class Solution {
                 sum=0;
                 sign=1;
             }else if(s.charAt(i)==')'){
-                sum = stack.pop()*sum;
-                sum +=stack.pop();
+                sum=stack.pop()*sum;
+                sum+=stack.pop();
             }
         }
         return sum;
