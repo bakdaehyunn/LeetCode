5edb3c150fde1a4f33e45ff297da87dde47f264e
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 19:15:51 +0900

    Time: 1 ms (100%), Space: 43.6 MB (29.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,31 +10,36 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null|lists.length==0)return null;
+        if(lists.length==0) return null;
         return helper(lists,0,lists.length-1);
     }
-    public ListNode helper(ListNode[] lists, int left, int right){
-        if(left==right)return lists[left];
-        int mid = left +(right-left)/2;
-        ListNode leftNode = helper(lists,left,mid);
-        ListNode rightNode = helper(lists,mid+1,right);
-        return merge(leftNode,rightNode);
+    public ListNode helper(ListNode[] lists,int start,int end){
+        if(start==end) return lists[start];
+        int mid = start +(end-start)/2;
+        ListNode left = helper(lists,start,mid);
+        ListNode right =helper(lists,mid+1,end);
+        return merge(left,right);
     }
-    public ListNode merge(ListNode left,ListNode right){
+    public ListNode merge(ListNode left, ListNode right){
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         while(left!=null&&right!=null){
             if(left.val<right.val){
-                current.next= left;
+                current.next = left;
                 left = left.next;
             }else{
                 current.next = right;
                 right = right.next;
             }
             current = current.next;
+
         }
-        if(left!=null)current.next = left;
-        if(right!=null) current.next = right;
+        if(left!=null){
+            current.next=left;
+        }
+        if(right!=null){
+            current.next=right;
+        }
         return dummy.next;
     }
 }
