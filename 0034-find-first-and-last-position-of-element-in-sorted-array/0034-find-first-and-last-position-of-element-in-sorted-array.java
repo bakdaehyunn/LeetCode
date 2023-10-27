b69f83773b8de519ebc58b3537618541f30de90e
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 20:15:23 +0900

    Time: 1 ms (23.54%), Space: 44.4 MB (49.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int first = findFirst(nums,target);
-        int second = findSecond(nums,target);
-        return new int[]{first,second};
+        int last = findLast(nums,target);
+        return new int[]{first,last};
     }
     public int findFirst(int[]nums, int target){
         int index=-1;
-        int left = 0;
-        int right = nums.length-1;
+        int left =0;
+        int right= nums.length-1;
         while(left<=right){
-            int mid = left + (right -left) /2;
+            int mid = left+(right-left);
             if(nums[mid]==target){
                 index=mid;
                 right = mid-1;
-            }else if(nums[mid]>target){
-                right = mid-1;
             }else if(nums[mid]<target){
                 left = mid+1;
+            }else if(nums[mid]>target){
+                right = mid-1;
             }
         }
         return index;
     }
-    public int findSecond(int[] nums , int target){
+    public int findLast(int[] nums,int target){
         int index=-1;
-        int left = 0;
-        int right = nums.length-1;
+        int left=0;
+        int right=nums.length-1;
         while(left<=right){
-            int mid = left + (right-left)/2;
+            int mid= left+(right-left)/2;
             if(nums[mid]==target){
-                index = mid;
+                index=mid;
                 left=mid+1;
             }else if(nums[mid]<target){
-                left = mid+1;
+                left =mid+1;
             }else if(nums[mid]>target){
-                right = mid-1;
+                right=mid-1;
             }
-
         }
         return index;
     }
