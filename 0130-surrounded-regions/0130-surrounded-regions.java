c600af90ba46fe012895e23de9ec9af91f344393
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 15:46:30 +0900

    Time: 2 ms (80.06%), Space: 43.7 MB (82.66%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,7 +2,7 @@ class Solution {
     public void solve(char[][] board) {
         for(int i=0;i<board.length;i++){
             dfs(board,i,0);
-            dfs(board,i,board[i].length-1);
+            dfs(board,i,board[0].length-1);
         }
         for(int j=0;j<board[0].length;j++){
             dfs(board,0,j);
@@ -15,10 +15,10 @@ class Solution {
             }
         }
     }
-    public void dfs(char[][] board,int i, int j){
-        if(i<0||i>board.length-1||j<0||j>board[0].length-1)return ;
+    public void dfs(char[][]board, int i,int j){
+        if(i<0||i>board.length-1||j<0||j>board[0].length-1)return;
         if(board[i][j]=='X'||board[i][j]=='*')return ;
-        board[i][j] = '*';
+        board[i][j]='*';
         dfs(board,i-1,j);
         dfs(board,i+1,j);
         dfs(board,i,j-1);
