6b3d9533ba0a91be896e08c03570c5d03c8dfb93
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 13:55:24 +0900

    Time: 0 ms (100%), Space: 43.7 MB (88.4%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -16,15 +16,15 @@
 class Solution {
     int max;
     public int maxPathSum(TreeNode root) {
-        max= Integer.MIN_VALUE;
+        max=Integer.MIN_VALUE;
         helper(root);
         return max;
     }
     public int helper(TreeNode root){
-        if(root == null)return 0;
+        if(root==null)return 0;
         int left = Math.max(0,helper(root.left));
         int right = Math.max(0,helper(root.right));
-        max = Math.max(max,left+right+root.val);
+        max=Math.max(max,left+right+root.val);
         return Math.max(left,right)+root.val;
     }
 }
