d8ecc6532ce59b4d66b7e8f1c37120e8a3e48abe
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 18:38:30 +0900

    Time: 157 ms (42.57%), Space: 40.6 MB (44.78%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,28 @@
 class Solution {
-    boolean[][] visited;
+    
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if(word.charAt(0)==board[i][j]&&backtrack(board,word,0,i,j)){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==word.charAt(0)&&backtrack(board,word,visited,i,j,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean backtrack(char[][] board, String word,int length, int i, int j){
-        if(length==word.length())return true;
-        if(i<0||i>board.length-1||j<0||j>board[i].length-1||word.charAt(length)!=board[i][j]||visited[i][j]){
-    return false;
-        } 
-        visited[i][j]= true;
-        if(backtrack(board,word,length+1,i-1,j)||
-        backtrack(board,word,length+1,i+1,j)||
-        backtrack(board,word,length+1,i,j-1)||
-        backtrack(board,word,length+1,i,j+1))return true;
+    public boolean backtrack(char[][] board,String word, boolean[][] visited, int i, int j, int index){
+        if(index==word.length()){
+            return true;
+        }
+        if(i<0||i>board.length-1||j<0||j>board[0].length-1||board[i][j]!=word.charAt(index)||visited[i][j])return false;
+        visited[i][j]=true;
+        if(backtrack(board,word,visited,i-1,j,index+1)||
+        backtrack(board,word,visited,i+1,j,index+1)||
+        backtrack(board,word,visited,i,j-1,index+1)||
+        backtrack(board,word,visited,i,j+1,index+1)){
+            return true;
+        }
         visited[i][j]=false;
         return false;
     }
