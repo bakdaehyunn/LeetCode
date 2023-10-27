eaa71cb609e523c6e254f2e7fec99d0c84447eb0
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-27 18:05:05 +0900

    Time: 13 ms (83.58%), Space: 92.3 MB (42.45%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result,new ArrayList<>(),1,n,k);
+        backtrack(result,new ArrayList<>(),n,k,1);
         return result;
     }
-    public void backtrack(List<List<Integer>> result, List<Integer> list, int start,int n,int k ){
+    public void backtrack(List<List<Integer>>result, List<Integer> list, int n, int k,int index){
         if(k==0){
             result.add(new ArrayList<>(list));
             return ;
         }
-        for(int i=start;i<=n;i++){
+        for(int i=index;i<=n;i++){
             list.add(i);
-            backtrack(result,list,i+1,n,k-1);
+            backtrack(result,list,n,k-1,i+1);
             list.remove(list.size()-1);
         }
     }
