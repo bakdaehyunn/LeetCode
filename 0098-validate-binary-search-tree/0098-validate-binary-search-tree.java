109bf3caabb3e8f4f2b7335b868453062c1e1acf
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-29 13:21:13 +0900

    Time: 0 ms (100%), Space: 43.9 MB (30.6%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -19,7 +19,8 @@ class Solution {
     }
     public boolean helper(TreeNode root, long min, long max){
         if(root ==null) return true;
-        if(min>=root.val||max<=root.val)return false;
+        if(root.val<=min||root.val>=max)return false;
         return helper(root.left,min,root.val)&&helper(root.right,root.val,max);
+
     }
 }
