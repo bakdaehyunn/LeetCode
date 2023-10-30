7fd3ec0987c9925dff5005969eb1b0f7f377dd06
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 20:12:45 +0900

    Time: 5 ms (55.12%), Space: 43.1 MB (80.47%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,13 +1,14 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
         String[] p = path.split("/");
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb=  new StringBuilder();
         for(int i=0;i<p.length;i++){
-            if(!stack.isEmpty()&&p[i].equals("..")) stack.pop();
-            else if(!p[i].equals("")&&!p[i].equals(".")&&!p[i].equals(".."))stack.push(p[i]);
+            if(!stack.isEmpty()&&p[i].equals(".."))stack.pop();
+            else if(!p[i].equals("")&&!p[i].equals("..")&&!p[i].equals("."))stack.push(p[i]);
         }
-        if(stack.isEmpty()) return "/";
+        if(stack.isEmpty())return "/";
+
         while(!stack.isEmpty()){
             sb.insert(0,"/"+stack.pop());
         }
