31aad6f8b5fb8fb80f9f542e829d90ee8aa79111
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 18:41:20 +0900

    Time: 2 ms (98.58%), Space: 42.8 MB (70.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] alphabets = new int[26];
-        for(int i=0;i<s.length();i++) alphabets[s.charAt(i)-'a'] +=1;
-        for(int i=0;i<t.length();i++) alphabets[t.charAt(i)-'a'] -=1;
-        for(int i:alphabets){
-            if(i!=0){
+        for(char c:s.toCharArray())alphabets[c-'a']++;
+        for(char c:t.toCharArray())alphabets[c-'a']--;
+        for(int alphabet:alphabets){
+            if(alphabet!=0){
                 return false;
             }
         }
