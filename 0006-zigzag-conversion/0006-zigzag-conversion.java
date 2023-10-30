1b1724397a238561a411e8f06346553af7c6735c
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 14:41:44 +0900

    Time: 4 ms (74.78%), Space: 44.2 MB (42.79%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,7 +2,7 @@ class Solution {
     public String convert(String s, int numRows) {
         StringBuilder[] sb = new StringBuilder[numRows];
         for(int i=0;i<numRows;i++){
-            sb[i] = new StringBuilder();
+            sb[i]= new StringBuilder();
         }
         int index=0;
         while(index<s.length()){
@@ -10,13 +10,12 @@ class Solution {
                 sb[i].append(s.charAt(index));
                 index++;
             }
-            for(int i=numRows-2;i>0&&index<s.length();i--){
+            for(int i=numRows-2;i>=1&&index<s.length();i--){
                 sb[i].append(s.charAt(index));
                 index++;
             }
-
         }
-        StringBuilder result =new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for(int i=0;i<numRows;i++){
             result.append(sb[i]);
         }
