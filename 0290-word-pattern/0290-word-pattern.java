9c8a457afa570914b247cd7fee758129b3e119ed
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 18:36:41 +0900

    Time: 0 ms (100%), Space: 40.6 MB (26.84%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] strs = s.split(" ");
-        if(strs.length!= pattern.length()){
-            return false;
-        }
+        if(pattern.length()!=strs.length)return false;
         Map<Character,String> charToStr = new HashMap<>();
         Map<String,Character> strToChar = new HashMap<>();
-        for(int i=0;i<pattern.length();i++){
+        for(int i=0;i<strs.length;i++){
             char c = pattern.charAt(i);
             String str = strs[i];
             if(!charToStr.containsKey(c)){
@@ -15,7 +13,7 @@ class Solution {
             if(!strToChar.containsKey(str)){
                 strToChar.put(str,c);
             }
-            if(!charToStr.get(c).equals(str)||!strToChar.get(str).equals(c)){
+            if(!charToStr.get(c).equals(str)||strToChar.get(str)!=c){
                 return false;
             }
         }
