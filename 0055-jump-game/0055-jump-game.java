dbce378ac7728a2098961bcc1a6c6703bceda243
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 12:46:47 +0900

    Time: 1 ms (99.99%), Space: 45.1 MB (6.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int end = nums.length-1;
+        int finalPos =nums.length-1;
         for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i]>=end){
-                end=i;
+            if(nums[i]+i>=finalPos){
+                finalPos = i;
             }
         }
-        return end==0;
+        return finalPos ==0;
     }
 }
