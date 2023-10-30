bd2d6b83ae0f35a3837430fa9cca20f3dff7ef08
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 16:03:10 +0900

    Time: 13 ms (33.72%), Space: 43.8 MB (22.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                char currentCell = board[i][j];
-                if(currentCell!='.'){
-                    if(!seen.add(currentCell+"row"+i)||
-                    !seen.add(currentCell+"column"+j)||
-                    !seen.add(currentCell+"cube"+i/3+"-"+j/3)){
+                if(board[i][j]!='.'){
+                    if(!set.add(board[i][j]+"row"+i)||
+                    !set.add(board[i][j]+"colum"+j)||
+                    !set.add(board[i][j]+"box"+i/3+"-"+j/3)){
                         return false;
                     }
                 }
