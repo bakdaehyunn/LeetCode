943a97d66dec9b1623b9153e2d0647a006d86d65
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-30 14:56:10 +0900

    Time: 1 ms (91.31%), Space: 40.2 MB (82.46%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,11 +3,9 @@ class Solution {
         int i=0;
         int j=0;
         while(i<s.length()&&j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-            }
+            if(s.charAt(i)==t.charAt(j))i++;
             j++;
         }
-        return i==s.length();  
+        return i==s.length();
     }
 }
