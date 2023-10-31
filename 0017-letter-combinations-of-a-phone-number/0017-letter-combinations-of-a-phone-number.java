9740208b8cab8a5b130c3284ce50ebbeaaf881e3
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-31 15:18:55 +0900

    Time: 0 ms (100%), Space: 40.7 MB (90.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,22 @@
 class Solution {
-    String[] map;
-    
+    String[] mapping;
     public List<String> letterCombinations(String digits) {
-        List<String> result  =new ArrayList<>();
+        List<String> result= new ArrayList<>();
+        mapping= new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         if(digits.length()==0)return result;
-        map=new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        backtrack(result,digits,new StringBuilder(),0);
+        backtrack(result,new StringBuilder(),0,digits);
         return result;
     }
-    public void backtrack(List<String> result,String digits,StringBuilder sb, int length){
-        if(digits.length()==length){
+    public void backtrack(List<String> result, StringBuilder sb, int index,String digits){
+        if(index==digits.length()){
             result.add(sb.toString());
-            return ;
+            return;
         }
-        char c = digits.charAt(length);
-        String s = map[c-'0'];
-        for(char ch:s.toCharArray()){
-            sb.append(ch);
-            backtrack(result, digits,sb,length+1);
+        char c = digits.charAt(index);
+        String str = mapping[c-'0'];
+        for(int i=0;i<str.length();i++){
+            sb.append(str.charAt(i));
+            backtrack(result,sb,index+1,digits);
             sb.deleteCharAt(sb.length()-1);
         }
     }
