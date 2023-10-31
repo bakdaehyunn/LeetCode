c825fc6c85d79f011aa7f2dee4147409e0dae344
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-31 14:04:03 +0900

    Time: 3 ms (72.16%), Space: 47.3 MB (75.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count =0;
+        int count=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
