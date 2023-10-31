1edbc4b337548c3f5bd54e5028160fd3e83f7371
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-10-31 16:01:38 +0900

    Time: 160 ms (40.33%), Space: 40.3 MB (76.45%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,28 @@
 class Solution {
-    
+    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==word.charAt(0)&&backtrack(board,word,visited,i,j,0)){
+        int a=board.length;
+        int b=board[0].length;
+        visited = new boolean[a][b];
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                if(board[i][j]==word.charAt(0)&&backtrack(board,word,i,j,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean backtrack(char[][] board,String word, boolean[][] visited, int i, int j, int index){
-        if(index==word.length()){
+    public boolean backtrack(char[][] board, String word, int i, int j,int index){
+        if(word.length()==index){
             return true;
         }
         if(i<0||i>board.length-1||j<0||j>board[0].length-1||board[i][j]!=word.charAt(index)||visited[i][j])return false;
         visited[i][j]=true;
-        if(backtrack(board,word,visited,i-1,j,index+1)||
-        backtrack(board,word,visited,i+1,j,index+1)||
-        backtrack(board,word,visited,i,j-1,index+1)||
-        backtrack(board,word,visited,i,j+1,index+1)){
+        if(backtrack(board,word,i-1,j,index+1)|
+        backtrack(board,word,i+1,j,index+1)||
+        backtrack(board,word,i,j-1,index+1)||
+        backtrack(board,word,i,j+1,index+1)){
             return true;
         }
         visited[i][j]=false;
