0bfc11e98924186680ec06e2d8e281e0037ac7f6
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-11-01 20:07:07 +0900

    Time: 0 ms (100%), Space: 40.1 MB (40.78%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,19 +14,19 @@
  * }
  */
 class Solution {
-    int result;
+    int result =0;
     public int sumNumbers(TreeNode root) {
         helper(root,0);
         return result;
     }
-    public void helper(TreeNode root,int sum){
-        if(root==null) return;
+    public void helper(TreeNode root, int sum){
+        if(root ==null)return ;
         sum = sum*10+root.val;
+        if(root.left==null&&root.right==null){
+            result+=sum;
+        }
         helper(root.left,sum);
         helper(root.right,sum);
-        if(root.left==null&&root.right==null){
-            result += sum;
-        }
-        
     }
+    
 }
