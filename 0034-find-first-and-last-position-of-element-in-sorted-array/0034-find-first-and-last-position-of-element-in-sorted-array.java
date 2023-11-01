5818aa361ab5e605dc0f680acae08949800ed8c8
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-11-01 16:58:34 +0900

    Time: 0 ms (100%), Space: 44.2 MB (65.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums,target);
-        int last = findLast(nums,target);
+        int first =findFirst(nums,target);
+        int last =findLast(nums,target);
         return new int[]{first,last};
     }
-    public int findFirst(int[]nums, int target){
+    public int findFirst(int[]nums,int target){
         int index=-1;
-        int left =0;
-        int right= nums.length-1;
+        int left=0;
+        int right = nums.length-1;
         while(left<=right){
-            int mid = left+(right-left);
+            int mid = left+(right-left)/2;
             if(nums[mid]==target){
                 index=mid;
-                right = mid-1;
+                right=mid-1;
             }else if(nums[mid]<target){
-                left = mid+1;
+                left= mid+1;
             }else if(nums[mid]>target){
-                right = mid-1;
+                right=mid-1;
             }
         }
         return index;
@@ -26,14 +26,14 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid= left+(right-left)/2;
+            int mid = left+(right-left)/2;
             if(nums[mid]==target){
                 index=mid;
                 left=mid+1;
-            }else if(nums[mid]<target){
-                left =mid+1;
             }else if(nums[mid]>target){
-                right=mid-1;
+                right= mid-1;
+            }else if(nums[mid]<target){
+                left= mid+1;
             }
         }
         return index;
