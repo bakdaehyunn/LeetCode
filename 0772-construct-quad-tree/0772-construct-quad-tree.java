2ef9a7f4d7c9c3d58df36b0c5bb417b9ab121d15
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-11-01 16:12:25 +0900

    Time: 0 ms (100%), Space: 43.6 MB (29.06%) - LeetHub

--- 0772-construct-quad-tree/0772-construct-quad-tree.java
+++ 0772-construct-quad-tree/0772-construct-quad-tree.java
@@ -42,22 +42,21 @@ class Solution {
     public Node construct(int[][] grid) {
         return helper(grid,0,0,grid.length);
     }
-    public Node helper(int[][] grid, int i, int j,int length){
+    public Node helper(int[][] grid,int i,int j,int length){
         if(isSame(grid,i,j,length)){
             return new Node(grid[i][j]==1,true);
-        }else{
-            Node root = new Node(false,false);
-            root.topLeft = helper(grid,i,j,length/2);
-            root.topRight = helper(grid,i,j+length/2,length/2);
-            root.bottomLeft = helper(grid,i+length/2,j,length/2);
-            root.bottomRight = helper(grid,i+length/2,j+length/2,length/2);
-            return root;
         }
+        Node root = new Node(false,false);
+        root.topLeft = helper(grid,i,j,length/2);
+        root.topRight = helper(grid,i,j+length/2,length/2);
+        root.bottomLeft = helper(grid,i+length/2,j,length/2);
+        root.bottomRight = helper(grid,i+length/2,j+length/2,length/2);
+        return root;
     }
-    public boolean isSame(int[][] grid, int i, int j,int length){
-        for(int x = i ;x<i+length;x++){
-            for(int y = j;y<j+length;y++){
-                if(grid[x][y]!= grid[i][j]){
+    public boolean isSame(int[][] grid, int i,int j, int length){
+        for(int x=i;x<i+length;x++){
+            for(int y=j;y<j+length;y++){
+                if(grid[x][y]!=grid[i][j]){
                     return false;
                 }
             }
