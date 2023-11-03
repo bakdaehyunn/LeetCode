322c6ad575bab01dc0b27ed295bbc11adb3c2d90
bakdaehyunn <99257977+bakdaehyunn@users.noreply.github.com>
2023-11-03 17:54:29 +0900

    Time: 1 ms (89.29%), Space: 43.4 MB (50.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,9 +4,10 @@ class Solution {
         backtrack(result,"",0,0,n);
         return result;
     }
-    public void backtrack(List<String> result, String str, int open, int close, int n){
+    public void backtrack(List<String> result, String str,int open,int close,int n){
         if(str.length()==n*2){
             result.add(str);
+            return ;
         }
         if(open<n)backtrack(result,str+"(",open+1,close,n);
         if(close<open)backtrack(result,str+")",open,close+1,n);
